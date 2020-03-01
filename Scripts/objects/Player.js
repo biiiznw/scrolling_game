00bd81626e8de2acfa8dc4690c8b06644068f63b
JASAHS <jasahshamsudheen10@gmail.com>
2020-03-01 14:12:17 -0500

    Keyboard Functionality working successfully

--- Scripts/objects/Player.js
+++ Scripts/objects/Player.js
@@ -25,6 +25,7 @@ var objects;
             return _this;
         }
         Object.defineProperty(Player.prototype, "died", {
+            //private _keyPosition:Vector2 = new Vector2(346, 0);
             // PUBLIC PROPERTIES
             set: function (status) {
                 this._died = status;
@@ -44,16 +45,35 @@ var objects;
             }
         };
         Player.prototype._move = function () {
-            //let newPositionX = util.Math.Lerp(this.position.x, this.stage.mouseX, 0.05);
-            this.position = new objects.Vector2(this.stage.mouseX, this._verticalPosition);
+            //this.position = new Vector2(this._keyPosition.x, this._verticalPosition);
+            if (config.Game.keyboardManager.moveLeft) {
+                this.x -= 5;
+            }
+            if (config.Game.keyboardManager.moveRight) {
+                this.x += 5;
+            }
+            this.position = new objects.Vector2(this.x, this._verticalPosition);
         };
+        // private _keyboardInput(event: KeyboardEvent) {
+        //     // PRESS LEFT ARROW OR 'A' KEY
+        //     if (event.keyCode == 37 || event.keyCode == 65) {
+        //        this._keyPosition.x -= 5;
+        //     }
+        //     // PRESS RIGHT ARROW OR 'D' KEY
+        //     else if (event.keyCode == 39 || event.keyCode == 68 ) {
+        //         this._keyPosition.x += 5;
+        //     }
+        //  }
         // PUBLIC METHODS
         Player.prototype.Start = function () {
-            this._verticalPosition = 760; // locked to the bottom of the screen
+            // this.x = 320;
+            // this.y = 430;
+            this._verticalPosition = 760;
         };
         Player.prototype.Update = function () {
             this._move();
             this._checkBounds();
+            // this._keyboardInput();
             // let mouseX = config.Game.STAGE.mouseX;
             // let mouseY = config.Game.STAGE.mouseY;
             // this.position = new Vector2(mouseX, mouseY);
--- Scripts/objects/Player.ts
+++ Scripts/objects/Player.ts
@@ -5,6 +5,8 @@ module objects
         // PRIVATE INSTANCE MEMBERS
         private _died:boolean = false;
         private _verticalPosition?:number;
+        //private _keyPosition:Vector2 = new Vector2(346, 0);
+        
         // PUBLIC PROPERTIES
         set died(status:boolean) {
             this._died = status;
@@ -35,17 +37,40 @@ module objects
         }      
         private _move(): void
         {
-            //let newPositionX = util.Math.Lerp(this.position.x, this.stage.mouseX, 0.05);
-            this.position = new Vector2(this.stage.mouseX, this._verticalPosition);
+            
+            //this.position = new Vector2(this._keyPosition.x, this._verticalPosition);
+            if(config.Game.keyboardManager.moveLeft){
+                this.x -=5;
+            }
+            if(config.Game.keyboardManager.moveRight){
+                this.x +=5;
+            }
+            this.position = new Vector2(this.x, this._verticalPosition);
         }
+        // private _keyboardInput(event: KeyboardEvent) {
+        //     // PRESS LEFT ARROW OR 'A' KEY
+        //     if (event.keyCode == 37 || event.keyCode == 65) {
+        //        this._keyPosition.x -= 5;
+        //     }
+            
+        //     // PRESS RIGHT ARROW OR 'D' KEY
+        //     else if (event.keyCode == 39 || event.keyCode == 68 ) {
+        //         this._keyPosition.x += 5;
+        //     }
+           
+        //  }
         // PUBLIC METHODS
         public Start(): void {
-            this._verticalPosition = 760; // locked to the bottom of the screen
+            // this.x = 320;
+            // this.y = 430;
+            this._verticalPosition = 760; 
         }
 
         public Update(): void {
             this._move();
             this._checkBounds();
+           // this._keyboardInput();
+
 
 
 
