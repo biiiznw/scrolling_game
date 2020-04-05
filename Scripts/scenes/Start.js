9ec5a990fae17962e26d1542ffe23d0d831dcd34
anyerin01 <anyerin01@gmail.com>
2020-04-05 19:40:43 -0400

    update

--- Scripts/scenes/Start.js
+++ Scripts/scenes/Start.js
@@ -9,6 +9,7 @@ var scenes;
             // initialization
             this._startButton = new objects.Button();
             this._background = new objects.Background();
+            this._startLabel = new objects.Label();
             // this._user = new objects.User();
             // this._firstSceen = new objects.Image();
             // this._tutorialButton = new objects.Button();
@@ -19,6 +20,7 @@ var scenes;
             //this._background = new objects.Background(config.Game.ASSETS.getResult("background"));
             // this._firstSceen = new objects.Image(config.Game.ASSETS.getResult("firstScreen"), 320, 400, true);
             this._startButton = new objects.Button(config.Game.ASSETS.getResult("startButton"), 320, 200, true);
+            this._startLabel = new objects.Label("SEE YOU AGAIN", "80px", "Impact, Charcoal, sans-serif", "#142950", 320, 150, true);
             this._background = new objects.Background(config.Game.ASSETS.getResult("back"));
             // this._tutorialButton = new objects.Button("Tuto", 550, 700, true);
             createjs.Sound.play("startSound");
@@ -29,6 +31,7 @@ var scenes;
         }
         Main() {
             this.addChild(this._background);
+            this.addChild(this._startLabel);
             // this.addChild(this._firstSceen);
             this.addChild(this._startButton);
             // this.addChild(this._tutorialButton);
--- Scripts/scenes/Start.ts
+++ Scripts/scenes/Start.ts
@@ -5,6 +5,7 @@ module scenes
         // PRIVATE INSTANCE MEMBERS
         private _startButton:objects.Button;
         private _background: objects.Background;
+        private _startLabel:objects.Label;
         // private _user: objects.User;
         // private _firstSceen: objects.Image;
         private _tutorialButton:objects.Button;
@@ -18,6 +19,7 @@ module scenes
             // initialization
             this._startButton = new objects.Button();
             this._background = new objects.Background();
+            this._startLabel = new objects.Label();
             // this._user = new objects.User();
             // this._firstSceen = new objects.Image();
             // this._tutorialButton = new objects.Button();
@@ -31,6 +33,7 @@ module scenes
             //this._background = new objects.Background(config.Game.ASSETS.getResult("background"));
             // this._firstSceen = new objects.Image(config.Game.ASSETS.getResult("firstScreen"), 320, 400, true);
             this._startButton = new objects.Button(config.Game.ASSETS.getResult("startButton"), 320, 200, true);
+            this._startLabel = new objects.Label("SEE YOU AGAIN", "80px","Impact, Charcoal, sans-serif", "#142950", 320, 150, true);
             this._background = new objects.Background(config.Game.ASSETS.getResult("back"));
             // this._tutorialButton = new objects.Button("Tuto", 550, 700, true);
             createjs.Sound.play("startSound");
@@ -44,6 +47,7 @@ module scenes
         
         public Main(): void {
             this.addChild(this._background);
+            this.addChild(this._startLabel);
             // this.addChild(this._firstSceen);
             this.addChild(this._startButton);
             // this.addChild(this._tutorialButton);
